f5c3802b8ca3112c8afeb0f18b704393575ea630
Thonatas Reis Borges <premiersoft-thonatas.borges@ailos.coop.br>
2022-04-22 18:16:03 -0400

    [CHANGE] FavoriteViewController

--- AnimeFav/Scenes/FavoriteViewController.swift
+++ AnimeFav/Scenes/FavoriteViewController.swift
@@ -8,17 +8,28 @@
 import UIKit
 
 class FavoriteViewController: UIViewController {
-
-    @IBOutlet weak var favoriteCollection: UICollectionView!
+    // MARK: - Views
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Favorites"
+        label.font = UIFont.systemFont(ofSize: 27, weight: .semibold)
+        label.textAlignment = .left
+        return label
+    }()
+    
+    private lazy var favoriteCollectionView: UICollectionView = {
+        let collectionView = UICollectionView()
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        return collectionView
+    }()
+    
     var cellSelected = 0
     var animeRepo = AnimeRepository()
     var animes: [Anime] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        favoriteCollection.delegate = self
-        favoriteCollection.dataSource = self
         animes = animeRepo.getFavoriteList()
         
         NotificationCenter.default.addObserver(self, selector: #selector(self.refresh), name: NSNotification.Name(rawValue: "changeFavorites"), object: nil)
@@ -27,7 +38,29 @@ class FavoriteViewController: UIViewController {
     
     @objc func refresh() {
         animes = animeRepo.getFavoriteList()
-        favoriteCollection.reloadData()
+        favoriteCollectionView.reloadData()
+    }
+}
+
+// MARK: - Layout
+extension FavoriteViewController: CodeView {
+    func buildViewHierarchy() {
+        self.view.addSubview(titleLabel)
+        self.view.addSubview(favoriteCollectionView)
+    }
+    
+    func buildConstraints() {
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(20)
+            make.leading.trailing.equalToSuperview().inset(20)
+            make.height.equalTo(50)
+        }
+        
+        favoriteCollectionView.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.top).offset(20)
+            make.leading.trailing.equalToSuperview().inset(10)
+            make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
+        }
     }
 }
 
@@ -53,17 +86,14 @@ extension FavoriteViewController: UICollectionViewDelegate {
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         cellSelected = indexPath.item
-        performSegue(withIdentifier: K.segueFavoriteToDetails, sender:self)
+        //performSegue(withIdentifier: K.segueFavoriteToDetails, sender:self)
     }
 
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        
         if segue.identifier == K.segueFavoriteToDetails {
-        
             let destinationVC = segue.destination as! AnimeDetailsViewController
             destinationVC.animeId = animes[cellSelected].id
-           
         }
     }
 }
