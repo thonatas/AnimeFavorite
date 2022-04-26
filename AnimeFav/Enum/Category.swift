887dbefcac83cea193bbf3f34566a67b107c4b02
Thonatas Reis Borges <premiersoft-thonatas.borges@ailos.coop.br>
2022-04-25 21:55:41 -0400

    [ADD] Category Enum

---
+++ AnimeFav/Enum/Category.swift
@@ -0,0 +1,14 @@
+//
+//  Category.swift
+//  AnimeFav
+//
+//  Created by Thonatas Borges on 4/25/22.
+//
+
+import Foundation
+
+enum Category: String, CaseIterable {
+    case members = "membros"
+    case score = "score"
+    case type = "tipo"
+}
